ef4a14a831c7e8670de571ef8cf3a09ea2345019
fiatjaf <fiatjaf@gmail.com>
2024-07-07 23:37:43 -0300

    nip11: signal support for deletions and count if those handlers exist.

--- handlers.go
+++ handlers.go
@@ -282,6 +282,14 @@ func (rl *Relay) HandleNIP11(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/nostr+json")
 
 	info := *rl.Info
+
+	if len(rl.DeleteEvent) > 0 {
+		info.SupportedNIPs = append(info.SupportedNIPs, 9)
+	}
+	if len(rl.CountEvents) > 0 {
+		info.SupportedNIPs = append(info.SupportedNIPs, 45)
+	}
+
 	for _, ovw := range rl.OverwriteRelayInformation {
 		info = ovw(r.Context(), r, info)
 	}
