09ca31832c91ad4231b61108fff559c1998ef550
Isaque Veras <isaqueveras@alu.ufc.br>
2025-02-08 15:52:54 -0300

    feat(routing): separating relays

--- examples/routing/main.go
+++ examples/routing/main.go
@@ -12,58 +12,48 @@ import (
 )
 
 func main() {
-	var r1, r2, r3 *khatru.Relay
+	router := khatru.NewRouter()
 
 	{
 		db := &slicestore.SliceStore{}
 		_ = db.Init()
-		r1 = khatru.NewRelay(db)
-		r1.WithCountEvents(db.CountEvents)
+		relay := khatru.NewRelay(db)
+		relay.WithCountEvents(db.CountEvents)
+
+		router.Route().
+			Req(func(filter nostr.Filter) bool { return slices.Contains(filter.Kinds, 30023) }).
+			Event(func(event *nostr.Event) bool { return event.Kind == 30023 }).
+			Relay(relay)
 	}
 
 	{
 		db := &sqlite3.SQLite3Backend{DatabaseURL: "/tmp/t"}
 		_ = db.Init()
-		r2 = khatru.NewRelay(db)
-		r2.WithCountEvents(db.CountEvents)
+		relay := khatru.NewRelay(db)
+		relay.WithCountEvents(db.CountEvents)
+
+		router.Route().
+			Req(func(filter nostr.Filter) bool {
+				return slices.Contains(filter.Kinds, 1) && slices.Contains(filter.Tags["t"], "spam")
+			}).
+			Event(func(event *nostr.Event) bool {
+				return event.Kind == 1 && event.Tags.GetFirst([]string{"t", "spam"}) != nil
+			}).
+			Relay(relay)
 	}
 
 	{
 		db := slicestore.SliceStore{}
 		_ = db.Init()
-		r3 = khatru.NewRelay()
-		r3.WithCountEvents(db.CountEvents)
+		relay := khatru.NewRelay()
+		relay.WithCountEvents(db.CountEvents)
+
+		router.Route().
+			Req(func(filter nostr.Filter) bool { return slices.Contains(filter.Kinds, 1) }).
+			Event(func(event *nostr.Event) bool { return event.Kind == 1 }).
+			Relay(relay)
 	}
 
-	router := khatru.NewRouter()
-
-	router.Route().
-		Req(func(filter nostr.Filter) bool {
-			return slices.Contains(filter.Kinds, 30023)
-		}).
-		Event(func(event *nostr.Event) bool {
-			return event.Kind == 30023
-		}).
-		Relay(r1)
-
-	router.Route().
-		Req(func(filter nostr.Filter) bool {
-			return slices.Contains(filter.Kinds, 1) && slices.Contains(filter.Tags["t"], "spam")
-		}).
-		Event(func(event *nostr.Event) bool {
-			return event.Kind == 1 && event.Tags.GetFirst([]string{"t", "spam"}) != nil
-		}).
-		Relay(r2)
-
-	router.Route().
-		Req(func(filter nostr.Filter) bool {
-			return slices.Contains(filter.Kinds, 1)
-		}).
-		Event(func(event *nostr.Event) bool {
-			return event.Kind == 1
-		}).
-		Relay(r3)
-
 	fmt.Println("running on :3334")
-	http.ListenAndServe(":3334", router)
+	_ = http.ListenAndServe(":3334", router)
 }
