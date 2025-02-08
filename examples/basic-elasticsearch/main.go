e6d28d9eddc43564809599f5317a507cbb8f3584
Isaque Veras <isaqueveras@alu.ufc.br>
2025-02-08 15:01:31 -0300

    refact(elasticsearch): init new relay with elasticsearch

--- examples/basic-elasticsearch/main.go
+++ examples/basic-elasticsearch/main.go
@@ -9,19 +9,16 @@ import (
 )
 
 func main() {
-	relay := khatru.NewRelay()
-
-	db := elasticsearch.ElasticsearchStorage{URL: ""}
+	db := &elasticsearch.ElasticsearchStorage{URL: ""}
 	if err := db.Init(); err != nil {
 		panic(err)
 	}
 
-	relay.StoreEvent = append(relay.StoreEvent, db.SaveEvent)
-	relay.QueryEvents = append(relay.QueryEvents, db.QueryEvents)
-	relay.CountEvents = append(relay.CountEvents, db.CountEvents)
-	relay.DeleteEvent = append(relay.DeleteEvent, db.DeleteEvent)
-	relay.ReplaceEvent = append(relay.ReplaceEvent, db.ReplaceEvent)
+	relay := khatru.NewRelay(db)
+	relay.WithCountEvents(db.CountEvents)
 
 	fmt.Println("running on :3334")
-	http.ListenAndServe(":3334", relay)
+	if err := http.ListenAndServe(":3334", relay); err != nil {
+		panic(err)
+	}
 }
