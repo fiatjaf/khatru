78b3b51c7091f5a66c365674f09feb23f2e28771
Isaque Veras <isaqueveras@alu.ufc.br>
2025-02-08 15:37:02 -0300

    refact(exclusive): init new relay with exclusive

--- examples/exclusive/main.go
+++ examples/exclusive/main.go
@@ -13,28 +13,19 @@ import (
 )
 
 func main() {
-	relay := khatru.NewRelay()
-
-	db := lmdb.LMDBBackend{Path: "/tmp/exclusive"}
+	db := &lmdb.LMDBBackend{Path: "/tmp/exclusive"}
 	os.MkdirAll(db.Path, 0755)
 	if err := db.Init(); err != nil {
 		panic(err)
 	}
 
-	relay.StoreEvent = append(relay.StoreEvent, db.SaveEvent)
-	relay.QueryEvents = append(relay.QueryEvents, db.QueryEvents)
-	relay.CountEvents = append(relay.CountEvents, db.CountEvents)
-	relay.DeleteEvent = append(relay.DeleteEvent, db.DeleteEvent)
+	relay := khatru.NewRelay()
+	relay.WithCountEvents(db.CountEvents)
 
 	relay.RejectEvent = append(relay.RejectEvent, policies.PreventTooManyIndexableTags(10, nil, nil))
 	relay.RejectFilter = append(relay.RejectFilter, policies.NoComplexFilters)
-
-	relay.OnEventSaved = append(relay.OnEventSaved, func(ctx context.Context, event *nostr.Event) {
-	})
+	relay.OnEventSaved = append(relay.OnEventSaved, func(ctx context.Context, event *nostr.Event) {})
 
 	fmt.Println("running on :3334")
 	http.ListenAndServe(":3334", relay)
-}
-
-func deleteStuffThatCanBeFoundElsewhere() {
 }
