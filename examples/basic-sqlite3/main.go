944b96f0828a9b6b65a4f47e01ee7f7d7f2bccd2
Isaque Veras <isaqueveras@alu.ufc.br>
2025-02-08 15:34:10 -0300

    refact(sqlite3): init new relay with sqlite3

--- examples/basic-sqlite3/main.go
+++ examples/basic-sqlite3/main.go
@@ -9,18 +9,13 @@ import (
 )
 
 func main() {
-	relay := khatru.NewRelay()
-
-	db := sqlite3.SQLite3Backend{DatabaseURL: "/tmp/khatru-sqlite-tmp"}
+	db := &sqlite3.SQLite3Backend{DatabaseURL: "/tmp/khatru-sqlite-tmp"}
 	if err := db.Init(); err != nil {
 		panic(err)
 	}
 
-	relay.StoreEvent = append(relay.StoreEvent, db.SaveEvent)
-	relay.QueryEvents = append(relay.QueryEvents, db.QueryEvents)
-	relay.CountEvents = append(relay.CountEvents, db.CountEvents)
-	relay.DeleteEvent = append(relay.DeleteEvent, db.DeleteEvent)
-	relay.ReplaceEvent = append(relay.ReplaceEvent, db.ReplaceEvent)
+	relay := khatru.NewRelay(db)
+	relay.WithCountEvents(db.CountEvents)
 
 	fmt.Println("running on :3334")
 	http.ListenAndServe(":3334", relay)
