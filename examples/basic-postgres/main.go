c6bbee6545e2b6b273f2fd370ae2bd05f046f717
Isaque Veras <isaqueveras@alu.ufc.br>
2025-02-08 15:33:41 -0300

    refact(postgres): init new relay with postgres

--- examples/basic-postgres/main.go
+++ examples/basic-postgres/main.go
@@ -9,18 +9,13 @@ import (
 )
 
 func main() {
-	relay := khatru.NewRelay()
-
-	db := postgresql.PostgresBackend{DatabaseURL: "postgresql://localhost:5432/tmp-khatru-relay"}
+	db := &postgresql.PostgresBackend{DatabaseURL: "postgresql://localhost:5432/tmp-khatru-relay"}
 	if err := db.Init(); err != nil {
 		panic(err)
 	}
 
-	relay.StoreEvent = append(relay.StoreEvent, db.SaveEvent)
-	relay.QueryEvents = append(relay.QueryEvents, db.QueryEvents)
-	relay.CountEvents = append(relay.CountEvents, db.CountEvents)
-	relay.DeleteEvent = append(relay.DeleteEvent, db.DeleteEvent)
-	relay.ReplaceEvent = append(relay.ReplaceEvent, db.ReplaceEvent)
+	relay := khatru.NewRelay(db)
+	relay.WithCountEvents(db.CountEvents)
 
 	fmt.Println("running on :3334")
 	http.ListenAndServe(":3334", relay)
